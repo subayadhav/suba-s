cdbe3370085204b61a5dd79a849ebb6c9e678a19
subayadhav <suba10499@gmail.com>
2017-06-28 14:46:23 +0530

    Update suba.c

--- suba.c
+++ suba.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("\n enter the number");
+scanf("%d",num);
+if(num>0)
+printf("\n positive");
+if(num<0)
+printf("\n negative");
+if(num==0)
+printf("\n zero");
+getch();
+}
